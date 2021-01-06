3e7eb60f901bbced3053f4989cc8e45809f7bcf4
Alexander Krog <moonfrogdk@gmail.com>
2021-01-06 18:56:21 +0100

    iss stuff

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import * as React from 'react'
-import { useEffect } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import * as p2pClientLib from 'celestium-p2p'
-import * as client from 'celestium-client-logic'
-import { io } from 'socket.io-client'
+import { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/home/home'
+import ISSFinder from './components/iss/iss'
 
 function App() {
-  useEffect(() => {
-    p2pClientLib.RTCSessionDescription = RTCSessionDescription
-    p2pClientLib.RTCIceCandidate = RTCIceCandidate
-    p2pClientLib.RTCPeerConnection = RTCPeerConnection
-    p2pClientLib.io = io;
-    client.p2pClientLib = p2pClientLib
-    client.storage = localStorage
-    import("celestium-wasm").then(x => {
-      client.walletLib = x;
-      console.log(client.initNewClient());
-      console.log(x.generateTestBlockchain(BigInt(Math.round(Date.now() / 1000))));
-      console.log("Current balance: " + x.getBalance());
-    });
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ISSFinder></ISSFinder>
   );
 }
+
 
 export default App;
