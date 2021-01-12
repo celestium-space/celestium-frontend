c7e484dd2bddb669089745061c0db55e1ae2b62b
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2021-01-12 04:53:48 +0100

    Msgpack for datapacking during webrtc transfer

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import {
 import Navbar from './components/navbar/navbar';
 import Store from './components/store/store';
 import { Container, Header } from 'semantic-ui-react';
+import * as msgpack from '@msgpack/msgpack';
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
     client.storage = localStorage;
     let wallet = await import("celestium-wasm");
     client.walletLib = wallet;
+    client.msgpack = msgpack;
     let instance = client.initNewClient();
     let pk = new Uint8Array([0x3, 0xFD, 0xA0, 0x6F, 0xE7, 0xF9, 0x24, 0xCA, 0x5E, 0xC4, 0x14, 0xB7, 0x2D, 0xB7, 0xB0, 0xC4, 0x14, 0x9B, 0xE1, 0x42, 0x0D, 0x13, 0xB4, 0x1F, 0xC6, 0xFA, 0x5F, 0xCD, 0x70, 0xA9, 0xBF, 0x51, 0xC9])
     let sk = new Uint8Array([0xDA, 0x2C, 0xA5, 0x40, 0x17, 0xEF, 0xC6, 0x9B, 0x1C, 0x05, 0x93, 0x1D, 0x56, 0x4E, 0x5D, 0x5D, 0xBA, 0x0F, 0xE0, 0xD8, 0x90, 0xD5, 0x2D, 0x46, 0x1C, 0x52, 0xB7, 0xFB, 0x1E, 0xD4, 0xB8, 0x36]);
@@ -87,8 +89,7 @@ function Send(props) {
       props.state.client.walletClientLib.generateInitBlockchain();
       console.log("Created init wallet; My balance:", props.state.client.walletClientLib.getBalance());
       console.log("Trying to sending blockchain...");
-      await props.state.client.sendBlockchain();
-      console.log("Blockchain sent!")
+      console.log(await props.state.client.sendBlockchain());
       //let pk = props.state.client.walletClientLib.getPK();
       //console.log(props.state.client.sendTransaction(pk, 100, 20));
     }
