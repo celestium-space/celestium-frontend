4e843b4467385db24fa204971a40200a569f454d
zanderdk <akrog@lyrebirds.dk>
2021-08-17 12:18:43 +0200

    fuck my life

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react'
-import * as p2pClientLib from "celestium-p2p";
-import * as client from "celestium-client-logic";
 import { io } from "socket.io-client";
 import {
   BrowserRouter as Router,
@@ -12,105 +10,105 @@ import {
   useParams
 } from "react-router-dom";
 import Navbar from './components/navbar/navbar';
-import Store from './components/store/store';
-<<<<<<< HEAD
+// import Store from './components/store/store';
 import Buy from './components/store/buy';
-import { Container, Form, Header } from 'semantic-ui-react';
-import ISSFinder from './components/iss/iss';
-=======
-import { Container, Header } from 'semantic-ui-react';
-import * as msgpack from '@msgpack/msgpack';
+import { Container, Form, FormButton, Header } from 'semantic-ui-react';
 
->>>>>>> f6c1dda361392822403cdc0e3757dd54635581e5
+var loadJS = function (url, implementationCode, location) {
+  //url is URL of external file, implementationCode is the code
+  //to be called from the file, location is the location to 
+  //insert the <script> element
+
+  var scriptTag = document.createElement('script');
+  scriptTag.src = url;
+
+  scriptTag.onload = implementationCode;
+  scriptTag.onreadystatechange = implementationCode;
+  location.appendChild(scriptTag);
+
+};
 
 function App() {
   let [state, setState] = useState({
-    client: null,
-    pk: null,
-    sk: null
+    wallet: null,
+    pool: null
   });
 
-  useEffect(async () => {
-    p2pClientLib.RTCSessionDescription = RTCSessionDescription;
-    p2pClientLib.RTCIceCandidate = RTCIceCandidate;
-    p2pClientLib.RTCPeerConnection = RTCPeerConnection;
-    p2pClientLib.io = io;
-    client.p2pClientLib = p2pClientLib;
-    client.storage = localStorage;
-    let wallet = await import("celestium-wasm");
-    client.walletLib = wallet;
-    client.msgpack = msgpack;
-    let instance = client.initNewClient();
-    let pk = new Uint8Array([0x3, 0xFD, 0xA0, 0x6F, 0xE7, 0xF9, 0x24, 0xCA, 0x5E, 0xC4, 0x14, 0xB7, 0x2D, 0xB7, 0xB0, 0xC4, 0x14, 0x9B, 0xE1, 0x42, 0x0D, 0x13, 0xB4, 0x1F, 0xC6, 0xFA, 0x5F, 0xCD, 0x70, 0xA9, 0xBF, 0x51, 0xC9])
-    let sk = new Uint8Array([0xDA, 0x2C, 0xA5, 0x40, 0x17, 0xEF, 0xC6, 0x9B, 0x1C, 0x05, 0x93, 0x1D, 0x56, 0x4E, 0x5D, 0x5D, 0xBA, 0x0F, 0xE0, 0xD8, 0x90, 0xD5, 0x2D, 0x46, 0x1C, 0x52, 0xB7, 0xFB, 0x1E, 0xD4, 0xB8, 0x36]);
-    setState({ client: instance, pk, sk });
-    // oconsole.log(instance);
-    // console.log(pk);
-    // console.log(instance.walletClientLib.generateTestBlockchain(pk, sk, 100));
-    // console.log(instance.walletClientLib.getBalance()); 
-    //console.log(instance)
+  window.app_handle = this;
 
-  }, [])
+  async function pik() {
+    console.log(state.wallet);
+    let k = state.wallet.create_and_mine_string_nft(new Uint8Array([0, 0, 0]));
+    console.log(k);
+  }
+
+    useEffect(() => {
+    function loadWasm() {
+      let msg = 'This demo requires a current version of Firefox (e.g., 79.0)';
+      if (typeof SharedArrayBuffer !== 'function') {
+        alert('this browser does not have SharedArrayBuffer support enabled' + '\n\n' + msg);
+        return
+      }
+      // Test for bulk memory operations with passive data segments
+      //  (module (memory 1) (data passive ""))
+      const buf = new Uint8Array([0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00,
+        0x05, 0x03, 0x01, 0x00, 0x01, 0x0b, 0x03, 0x01, 0x01, 0x00]);
+      if (!WebAssembly.validate(buf)) {
+        alert('this browser does not support passive wasm memory, demo does not work' + '\n\n' + msg);
+        return
+      }
+
+      wasm_bindgen('./celestium_wasm_bg.wasm')
+        .then(run)
+        .catch(console.error);
+    }
+
+    loadWasm();
+
+    const { WASMWallet, WorkerPool } = wasm_bindgen;
+
+    function run() {
+      // The maximal concurrency of our web worker pool is `hardwareConcurrency`,
+      // so set that up here and this ideally is the only location we create web
+      // workers.
+      let pool = new WorkerPool(navigator.hardwareConcurrency);
+
+      // Configure various buttons and such.
+      function fuck() {
+        if (!localStorage.getItem("pk_bin") || !localStorage.getItem("sk_bin")) {
+          console.log("No keys!");
+          localStorage.setItem("pk_bin", "{\"0\":2,\"1\":159,\"2\":144,\"3\":13,\"4\":103,\"5\":147,\"6\":50,\"7\":58,\"8\":202,\"9\":16,\"10\":36,\"11\":110,\"12\":161,\"13\":51,\"14\":214,\"15\":167,\"16\":3,\"17\":1,\"18\":179,\"19\":103,\"20\":192,\"21\":221,\"22\":59,\"23\":249,\"24\":207,\"25\":7,\"26\":108,\"27\":132,\"28\":42,\"29\":246,\"30\":126,\"31\":67,\"32\":227}");
+          localStorage.setItem("sk_bin", "{\"0\":205,\"1\":37,\"2\":8,\"3\":20,\"4\":118,\"5\":209,\"6\":72,\"7\":27,\"8\":2,\"9\":128,\"10\":253,\"11\":42,\"12\":184,\"13\":79,\"14\":120,\"15\":133,\"16\":90,\"17\":63,\"18\":186,\"19\":230,\"20\":226,\"21\":192,\"22\":243,\"23\":245,\"24\":205,\"25\":236,\"26\":240,\"27\":248,\"28\":175,\"29\":73,\"30\":21,\"31\":168}")
+        }
+        let pk = new Uint8Array(Object.values(JSON.parse(localStorage.getItem("pk_bin"))));
+        let sk = new Uint8Array(Object.values(JSON.parse(localStorage.getItem("sk_bin"))));
+        let wasm_wallet = new WASMWallet(pk, sk, pool, parseInt(navigator.hardwareConcurrency));
+        let message = new Uint8Array([0x48, 0x0e, 0x6c, 0x6c, 0x6f]);
+        let transaction = wasm_wallet.create_and_mine_string_nft(message);
+        console.log(transaction);
+
+      }
+
+      fuck();
+    }
+  }, []);
 
   return (
     <Router>
       <Switch>
-        <Route path="/send">
-          <Send state={state}></Send>
-        </Route>
-        <Route path="/iss">
-          <ISSFinder></ISSFinder>
-          <Navbar active="iss"></Navbar>
-        </Route>
-        <Route path="/store">
-          <Store></Store>
-          <Navbar active="store"></Navbar>
-        </Route>
-        <Route path="/buy">
-          <Buy></Buy>
-          <Navbar active="store"></Navbar>
-        </Route>
         <Route path="/">
-          <Recv state={state} />
-          <Navbar active="store"></Navbar>
+          <div>
+            <h1>{state.wallet ? state.wallet.name : ""}</h1>
+            <button onClick={async (x) => {
+              setTimeout(pik, 0);
+            }}>
+              Activate Lasers
+            </button>
+          </div>
         </Route>
       </Switch>
     </Router>
   );
 }
-
-function Recv(props) {
-  useEffect(async () => {
-    if (props.state.client) {
-      props.state.client.walletClientLib.initWithKeyPair(props.state.pk, props.state.sk);
-      console.log("Created empty wallet; Blockchain len:", props.state.client.walletClientLib.blockchainLength());
-      console.log("Asking for blockchain...");
-      await props.state.client.receiveBlockchain();
-      console.log("Got blockchain; Blockchain len:", props.state.client.walletClientLib.blockchainLength());
-      console.log("My balance:", props.state.client.walletClientLib.getBalance());
-      //console.log(await props.state.client.receiveTransaction());
-      //await props.state.client.mineOffChainTransactions();
-    }
-  }, [props.state.client])
-  return <h1>recv</h1>;
-}
-
-function Send(props) {
-  useEffect(async () => {
-    if (props.state.client) {
-      props.state.client.walletClientLib.generateInitBlockchain();
-      console.log("Created init wallet; My balance:", props.state.client.walletClientLib.getBalance());
-      props.state.client.walletClientLib.sendCoins(props.state.pk, 100, 20);
-      console.log("Sent 100CEL (20), starting miner...");
-      await props.state.client.mineOffChainTransactions();
-      console.log("Blocks mined! Len:", props.state.client.walletClientLib.blockchainLength(), ". ", props.state.client.walletClientLib.getBalance(), ". Trying to send blockchain...");
-      console.log(await props.state.client.sendBlockchain());
-      //let pk = props.state.client.walletClientLib.getPK();
-      //console.log(props.state.client.sendTransaction(pk, 100, 20));
-    }
-  }, [props.state])
-  return <h1>send</h1>;
-}
-
 
 export default App;
--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,44 +1,18 @@
 
 import React, { Component, useEffect, useRef, useState } from 'react'
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+import { Stage, Layer, Rect, Circle, Image } from 'react-konva';
 import useImage from 'use-image';
 import UrlImage from './img';
 
-function Store(props) {
-
-    let POVX = window.screen.width*1.5;
-    let POVY = window.screen.height*1.5;
+function Pixels(props) {
 
     let stageRef = useRef();
 
-    let [state, setState] = useState({ objThing: { 'key1': [0, 0] } });
 
-    let update = true;
+    let POVX = window.screen.width;
+    let POVY = window.screen.height;
 
-    let loadNewImgs = (x, y) => {
-        x -= window.screen.width / 2;
-        y -= window.screen.height / 2;
-        if (update) {
-            let newImgs = state.objThing;
-
-            for (let j = y - POVY; j < y + POVY; j += 128) {
-                for (let i = x - POVX; i < x + POVX; i += 128) {
-                    let xindex = Math.floor(-i / 128);
-                    let yindex = Math.floor(-j / 128);
-                    let imgx = xindex * 128;
-                    let imgy = yindex * 128;
-                    let r = Math.random().toString(36).substring(7);
-                    let url = "/api/images?" + r; //change this
-                    let key = "x:" + xindex.toString() + "y:" + yindex.toString();
-                    let newThing = [imgx, imgy, url];
-                    if (!newImgs[key]) {
-                        newImgs[key] = newThing;
-                    }
-                }
-            }
-            setState({ objThing: newImgs });
-        }
-    }
+    let [state, setState] = useState({ lst: [] });
 
     useEffect(() => {
         console.log(stageRef);
@@ -46,25 +20,105 @@ function Store(props) {
             let x = e.currentTarget.attrs.x;
             let y = e.currentTarget.attrs.y;
             console.log("x:", x, "y:", y);
-            loadNewImgs(x, y);
         });
-        loadNewImgs(0, 0);
+
+        let img = create_image(16, 16, [...Array(256).keys()].map(x => [0xff, 0, 0]).flat());
+        console.log(img);
+
     }, [])
 
     return (
         <div>
-            <Stage ref={stageRef} draggable={true} width={window.innerWidth} height={window.innerHeight}>
+            <Stage ref={stageRef} scaleX={1} scaleY={1} draggable={true} width={window.innerWidth} height={window.innerHeight}>
                 <Layer >
-                    {
-                        Object.entries(state.objThing).map((xx) => {
-                            let [key, value] = xx;
-                            let [x, y, url] = value;
-                            return <UrlImage key={xx} x={x} y={y} url={url} ></UrlImage>
-                        })
-                    }
+                    {/* <Image x={0} y={0} image={create_image(16, 16, [...Array(256).keys()].map(x => [0xff, 0, 0]).flat())} /> */}
                 </Layer>
             </Stage>
+            {/* {
+                create_image(16, 16, [...Array(256).keys()].map(x => [0xff, 0, 0]).flat())
+            } */}
         </div>)
+
+
+    function create_image(width, height, data) {
+        console.log(data);
+        const header_size = 70;
+
+        const image_size = width * height * 4;
+
+        const arr = new Uint8Array(header_size + image_size);
+        const view = new DataView(arr.buffer);
+
+        // File Header
+
+        // BM magic number.
+        view.setUint16(0, 0x424D, false);
+        // File size.
+        view.setUint32(2, arr.length, true);
+        // Offset to image data.
+        view.setUint32(10, header_size, true);
+
+        // BITMAPINFOHEADER
+
+        // Size of BITMAPINFOHEADER
+        view.setUint32(14, 40, true);
+        // Width
+        view.setInt32(18, width, true);
+        // Height (signed because negative values flip
+        // the image vertically).
+        view.setInt32(22, height, true);
+        // Number of colour planes (colours stored as
+        // separate images; must be 1).
+        view.setUint16(26, 1, true);
+        // Bits per pixel.
+        view.setUint16(28, 32, true);
+        // Compression method, 6 = BI_ALPHABITFIELDS
+        view.setUint32(30, 6, true);
+        // Image size in bytes.
+        view.setUint32(34, image_size, true);
+        // Horizontal resolution, pixels per metre.
+        // This will be unused in this situation.
+        view.setInt32(38, 10000, true);
+        // Vertical resolution, pixels per metre.
+        view.setInt32(42, 10000, true);
+        // Number of colours. 0 = all
+        view.setUint32(46, 0, true);
+        // Number of important colours. 0 = all
+        view.setUint32(50, 0, true);
+
+        // Colour table. Because we used BI_ALPHABITFIELDS
+        // this specifies the R, G, B and A bitmasks.
+
+        // Red
+        view.setUint32(54, 0x000000FF, true);
+        // Green
+        view.setUint32(58, 0x0000FF00, true);
+        // Blue
+        view.setUint32(62, 0x00FF0000, true);
+        // Alpha
+        view.setUint32(66, 0xFF000000, true);
+
+        let i = 0;
+        for (let w = 0; w < width; ++w) {
+            for (let h = 0; h < height; ++h) {
+                const offset = header_size + (h * width + w) * 4;
+                arr[offset + 0] = data[i];
+                arr[offset + 1] = data[i + 1];
+                arr[offset + 2] = data[i + 2];
+                arr[offset + 3] = 255;
+                i += 3;
+            }
+        }
+
+        // const blob = new Blob([arr], { type: "image/bmp" });
+        // var urlCreator = window.URL || window.webkitURL;
+        // var imageUrl = urlCreator.createObjectURL(blob);
+        // let img = new window.Image(width, height);
+        // img.src = imageUrl;
+        var b64encoded = btoa(String.fromCharCode.apply(null, arr));
+        // let src = "data:image/png;base64," + b64encoded;
+        return b64encoded;
+    }
 }
 
-export default Store;
+export default Pixels;
