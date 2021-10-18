ee9a93d862d4c80e9015e881382a34c39aa67faa
hutli <jensstaermose@hotmail.com>
2021-10-18 12:04:18 +0200

    Universal celestium project info page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { useState, useEffect, useRef } from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { range } from "./utils";
 import Grid from "./components/grid/Grid";
-import PixelControls from "./components/pixelcontrols/PixelControls";
 import LogicHandler from "./logicHandler";
 import Store from "./components/store/Store";
 import Navbar from "./components/navbar/Navbar";
 import "./App.css";
 import Wallet from "./components/wallet/Wallet";
+import FrontPageInfo from "./components/popups/FrontPageInfo";
+import { Button } from "semantic-ui-react";
 
 function App() {
   window.app_handle = this;
@@ -35,28 +36,72 @@ function App() {
     );
     logic.getSocket();
     setState({ logic: logic });
+
+    let showCelestiumInfoOnStart =
+      localStorage.getItem("showCelestiumInfoOnStart") != "false";
+    setState({
+      showInfo: showCelestiumInfoOnStart,
+      showCelestiumInfoOnStart: showCelestiumInfoOnStart,
+    });
   }, [location]);
 
+  let style = {
+    display: "none",
+  };
+  if (!state.showInfo) {
+    style = {};
+  }
   return (
-    <Switch>
-      <Route path="/store">
-        <div className="content">
-          <Store ref={store} logic={logic}></Store>
-        </div>
-        <Navbar active="store"></Navbar>
-      </Route>
-      <Route path="/wallet">
-        <div className="content">
-          <Wallet></Wallet>
-        </div>
-        <Navbar active="wallet"></Navbar>
-      </Route>
-      <Route path="/">
-        <Grid ref={grid}></Grid>
-        <PixelControls ref={pixelControls}></PixelControls>
-        <Navbar active="grid"></Navbar>
-      </Route>
-    </Switch>
+    <div>
+      <div style={style}>
+        <Switch>
+          <Route path="/store">
+            <div className="content">
+              <Store ref={store} logic={logic}></Store>
+            </div>
+            <Navbar active="store"></Navbar>
+          </Route>
+          <Route path="/wallet">
+            <Wallet></Wallet>
+            <Navbar active="wallet"></Navbar>
+          </Route>
+          <Route path="/">
+            <Grid ref={grid} pixelControls={pixelControls}></Grid>
+            <Navbar active="grid"></Navbar>
+          </Route>
+        </Switch>
+        <Button
+          circular
+          className="question-btn"
+          icon="question circle outline"
+          style={{
+            padding: "0",
+            fontSize: "40px",
+            backgroundColor: "#333333",
+            color: "white",
+            bottom: "11px",
+            right: "10px",
+            position: "absolute",
+          }}
+          onClick={() => {
+            setState({ showInfo: true });
+          }}
+        />
+      </div>
+      <FrontPageInfo
+        showCelestiumInfo={state.showInfo}
+        setShowCelestiumInfo={(change) => {
+          console.log("show change!");
+          setState({ showInfo: change });
+        }}
+        showCelestiumInfoOnStart={state.showCelestiumInfoOnStart}
+        setShowCelestiumInfoOnStart={(change) => {
+          console.log("show on start change!");
+          setState({ showCelestiumInfoOnStart: change });
+          localStorage.setItem("showCelestiumInfoOnStart", change);
+        }}
+      ></FrontPageInfo>
+    </div>
   );
 }
 
