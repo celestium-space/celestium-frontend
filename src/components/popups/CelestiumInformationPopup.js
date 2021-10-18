d74cc14da7e8d2838f18def1112227a9a0f2b336
hutli <jensstaermose@hotmail.com>
2021-10-18 12:14:14 +0200

    Wallet info pop countdown

--- src/components/popups/CelestiumInformationPopup.js
+++ src/components/popups/CelestiumInformationPopup.js
@@ -2,8 +2,10 @@ import React from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import "./MyPopups.css";
+import Countdown from "react-countdown";
+import CelestiumLogo from "../images/CelestiumLogo";
 
-function MyPopup(props) {
+export default function CelestiumInformationPopup(props) {
   return (
     <Popup open={props.open} closeOnDocumentClick modal nested>
       {(close) => (
@@ -11,21 +13,30 @@ function MyPopup(props) {
           <button className="close" onClick={close}>
             &times;
           </button>
-
           <div className="header">Welcome to the Celestium Canvas</div>
           <div className="content">
             The image created here will be sent to the ISS at:
           </div>
-          <div className="header2">16:42.00 EST</div>
-          <div className="header3">September 30, 2021</div>
+          <Countdown
+            daysInHours={true}
+            zeroPadTime={2}
+            date="2021-10-17T12:00:00+02:00"
+            renderer={({ hours, minutes, seconds, completed }) => {
+              return (
+                <div className="header2">
+                  {hours}:{minutes}:{seconds}
+                </div>
+              );
+            }}
+          />
+          <div className="header3">12:00 EST, October 17, 2021</div>
           <div className="content" style={{ width: "280px" }}>
             Click anywhere on the canvas to contribute, by placing your pixel
-            and mining a Celestium Token (C)
+            and mine a Celestium Token (
+            <CelestiumLogo lineHeight="14pt" />)
           </div>
         </div>
       )}
     </Popup>
   );
 }
-
-export default MyPopup;
