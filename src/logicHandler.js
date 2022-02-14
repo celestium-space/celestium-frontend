28ca5ac8df1ed1f892ed976613e50f31bf7e5cef
hutli <jensstaermose@hotmail.com>
2022-02-14 18:07:20 +0100

    Fixed mined transactions send error

--- src/logicHandler.js
+++ src/logicHandler.js
@@ -311,9 +311,9 @@ class LogicHandler {
           for (let i = 0; i < pixel_nft_transaction.byteLength; i++) {
             transactions_message[i + 1] = pixel_nft_transaction[i];
           }
-          for (let i = 0; i < transactions_message.byteLength; i++) {
-            transactions_message[i + pixel_nft_transaction.byteLength + 1] =
-              transactions_message[i];
+          for (let i = 0; i < katjing_transaction.byteLength; i++) {
+            transactions_message[i + katjing_transaction.byteLength + 1] =
+              katjing_transaction[i];
           }
 
           console.log(`Sending mined transactions`);
