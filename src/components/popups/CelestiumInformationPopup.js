a0691977add22bd8a1d7c68e48623b50f43f0ec4
hutli <jensstaermose@hotmail.com>
2022-03-31 20:12:59 +0200

    REACT_APP_FREEZE_BLOCKCHAIN environment variable

--- src/components/popups/CelestiumInformationPopup.js
+++ src/components/popups/CelestiumInformationPopup.js
@@ -28,61 +28,73 @@ export default function CelestiumInformationPopup(props) {
             &times;
           </button>
           <div className="header">Welcome to the Celestium Canvas</div>
-          <div className="content" style={{ maxWidth: "300px" }}>
-            Final date has now been planned by NASA and the image created here
-            will be frozen in:
-            {/*The image created here will be sent to the International Space
+          (!process.env.REACT_APP_FREEZE_BLOCKCHAIN ?
+          <div>
+            <div className="content" style={{ maxWidth: "300px" }}>
+              Final date has now been planned by NASA and the image created here
+              will be frozen in:
+              {/*The image created here will be sent to the International Space
             Station.
             <br />
             <br />
             The specific date has not yet been set, but as soon as we know a{" "}
             <i>countdown</i> will start here. However,{" "}
             <i>pixels placed now still counts!</i>*/}
-          </div>
-          <SemanticPopup
-            style={{ height: "100%", textAlign: "center" }}
-            position="top center"
-            content="This time is subject to change if deemed nessesary by NASA"
-            trigger={
-              <div className="modal">
-                <Countdown
-                  daysInHours={true}
-                  date={launch}
-                  renderer={({
-                    total,
-                    days,
-                    hours,
-                    minutes,
-                    seconds,
-                    milliseconds,
-                    completed,
-                  }) => {
-                    return (
-                      <div className="header2">
-                        {days ? `${days} Days - ` : ""}
-                        {hours.toString().padStart(2, "0")}:
-                        {minutes.toString().padStart(2, "0")}:
-                        {seconds.toString().padStart(2, "0")}*
-                      </div>
-                    );
-                  }}
-                />
-                <div className="header3">
-                  {launch.toLocaleString(undefined, options)}
+            </div>
+            <SemanticPopup
+              style={{ height: "100%", textAlign: "center" }}
+              position="top center"
+              content="This time is subject to change if deemed nessesary by NASA"
+              trigger={
+                <div className="modal">
+                  <Countdown
+                    daysInHours={true}
+                    date={launch}
+                    renderer={({
+                      total,
+                      days,
+                      hours,
+                      minutes,
+                      seconds,
+                      milliseconds,
+                      completed,
+                    }) => {
+                      return (
+                        <div className="header2">
+                          {days ? `${days} Days - ` : ""}
+                          {hours.toString().padStart(2, "0")}:
+                          {minutes.toString().padStart(2, "0")}:
+                          {seconds.toString().padStart(2, "0")}*
+                        </div>
+                      );
+                    }}
+                  />
+                  <div className="header3">
+                    {launch.toLocaleString(undefined, options)}
+                  </div>
                 </div>
-              </div>
-            }
-          />
-          <br />
-          <div className="content" style={{ maxWidth: "300px" }}>
-            The block will be checked by NASA cyber security and sent to the ISS
-            on 5th of April 2022.
+              }
+            />
+            <br />
+            <div className="content" style={{ maxWidth: "300px" }}>
+              The block will be checked by NASA cyber security and sent to the
+              ISS on 5th of April 2022.
+            </div>
+            <div className="content" style={{ maxWidth: "300px" }}>
+              Click anywhere on the canvas to contribute, by placing your pixel
+              and mine a Celestium Token (
+              <CelestiumLogo lineHeight="14pt" />)
+            </div>
           </div>
+          :
           <div className="content" style={{ maxWidth: "300px" }}>
-            Click anywhere on the canvas to contribute, by placing your pixel
-            and mine a Celestium Token (
-            <CelestiumLogo lineHeight="14pt" />)
+            Celestium is now ower. The artwork you see here is the one that is
+            sent to the ISS on the 5th of April 2022. You can still browse the
+            canvas, your wallet and the asteroids but no more transactions will
+            be accepted until after the blocchain and canvas has been sent to
+            the ISS.
           </div>
+          )
         </div>
       )}
     </Popup>
